eb2c30e9cda13bc3266a9e53c14f20e9034c530a
proyectista <proyectista@IIT-PFC1-PL3.upcont.es>
2019-06-07 13:36:03 +0200

    Almost fixed

--- R/SensAnalysisMLP.R
+++ R/SensAnalysisMLP.R
@@ -917,7 +917,10 @@ SensAnalysisMLP.nnet <- function(MLP.fit, .returnSens = TRUE, trData, preProc = 
   finalModel$n <- MLP.fit$n
   finalModel$wts <- MLP.fit$wts
   finalModel$coefnames <- MLP.fit$coefnames
-  names(trData)[!names(trData) %in% attr(MLP.fit$terms,"term.labels")] <- ".outcome"
+  if(!any(names(trData) == ".outcome")){
+    names(trData)[!names(trData) %in% attr(MLP.fit$terms,"term.labels")] <- ".outcome"
+  }
+
   actfun <- c("linear","sigmoid",
               ifelse(is.factor(trData$.outcome),"sigmoid","linear"))
   SensAnalysisMLP.default(finalModel,
