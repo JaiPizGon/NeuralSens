df4547ef4c8e3483773f341eff76737185df76e9
Jaime Pizarroso Gonzalo <jpizarroso@comillas.edu>
2020-04-25 12:36:58 +0200

    Solve bugs for binary classification

--- R/ComputeSensMeasures.R
+++ R/ComputeSensMeasures.R
@@ -129,7 +129,7 @@ ComputeSensMeasures <- function(sens) {
     rs[[i]] <- der[,,i]
   }
   if (is.factor(sens$trData$.outcome)) {
-    names(out) <- make.names(unique(sens$trData$.outcome), unique = TRUE)
+    names(out) <- make.names(unique(sens$trData$.outcome), unique = TRUE)[1:length(out)]
   } else if (!is.null(sens$output_name)) {
     names(out) <- sens$output_name
   }
