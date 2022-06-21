16739fc140bea68da3ad4648ad7615ea0696d2bc
Jaime Pizarroso Gonzalo <jpizarroso@comillas.edu>
2022-06-21 22:43:11 +0200

    [python]: Add tests to package using pytest

--- python/tests/test_activation_functions.py
+++ python/tests/test_activation_functions.py
@@ -4,21 +4,106 @@
 
 import pytest
 
+from neuralsens import activation_functions as ac_f
 
-from neuralsens import neuralsens
+def test_fill_diagonal_torch():
+    """pytest test to check if torch tensor diagonal can be customized."""
+    try:
+        import torch 
+        # Create zero tensor and change its diagonal
+        z = torch.zeros((3,3,3))
+        diagonal = torch.Tensor([1, 2, -1])
+        z = ac_f.fill_diagonal_torch(z, diagonal)
+        
+        # Create tensor to check above operation
+        z_check = torch.Tensor([[[ 1.,  0.,  0.],
+                                 [ 0.,  0.,  0.],
+                                 [ 0.,  0.,  0.]],
+                                [[ 0.,  0.,  0.],
+                                 [ 0.,  2.,  0.],
+                                 [ 0.,  0.,  0.]],
+                                [[ 0.,  0.,  0.],
+                                 [ 0.,  0.,  0.],
+                                 [ 0.,  0., -1.]]])
 
+        assert torch.equal(z, z_check), "Function to fill diagonal of 3D-Tensor not working as expected"
+        
+        # Create zero tensor and change its diagonal
+        z = torch.zeros((4,4,4,4))
+        diagonal = torch.Tensor([5, -7, 0, 2])
+        z = ac_f.fill_diagonal_torch(z, diagonal)
+        
+        # Create tensor to check above operation
+        z_check = torch.Tensor([[[[ 5.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]]],
+                                [[[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0., -7.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]]],
+                                [[[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]]],
+                                [[[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.]],
+                                 [[ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  0.],
+                                  [ 0.,  0.,  0.,  2.]]]])
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+        # Check tensors are equal
+        assert torch.equal(z, z_check), "Function to fill diagonal of 4D-Tensor not working as expected"
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    except ImportError:
+        print(
+            "torch installation could not be found, test shall not be passed"
+        )
+    
