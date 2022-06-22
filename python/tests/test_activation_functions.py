3b3112353c8d486f48c0ad8298a06c021df3f433
Jaime Pizarroso Gonzalo <jpizarroso@comillas.edu>
2022-06-22 20:42:42 +0200

    [python]: update tests file

--- python/tests/test_activation_functions.py
+++ python/tests/test_activation_functions.py
@@ -106,4 +106,57 @@ def test_fill_diagonal_torch():
         print(
             "torch installation could not be found, test shall not be passed"
         )
-    
+
+def test_activation_function_torch():
+    """ Test to check if activation function performs as expected for pytorch Tensor. """
+    try:
+        from torch import sigmoid, tanh, relu, rand, equal
+        from random import randrange
+        def identity(x):
+            return x
+        act_fct = {"logistic": sigmoid,
+                    "sigmoid": sigmoid,
+                    "expit": sigmoid,
+                    "identity": identity,
+                    "tanh": tanh,
+                    "relu": relu}
+        for af, fct in act_fct.items(): 
+            random_size = randrange(2, 20)
+            acf = ac_f.activation_function(af, use_torch=True)
+            tns = rand(random_size)
+            tns_acf = acf(tns)
+            tns_check = tns
+            for i in range(random_size):
+                tns_check[i] = fct(tns[i])
+            assert equal(tns_acf, tns_check), "Activation function of the package not working correctly"
+        
+    except ImportError:
+        print(
+            "torch installation could not be found, test shall not be passed"
+        )
+
+def test_activation_function_numpy():
+    """ Test to check if activation function performs as expected for numpy vector. """
+    from numpy import tanh, array_equal
+    from numpy.random import rand
+    from scipy.special import expit
+    from random import randrange
+    def identity(x):
+        return x
+    def relu(x):
+        return x if x > 0 else 0
+    act_fct = {"logistic": expit,
+                "sigmoid": expit,
+                "expit": expit,
+                "identity": identity,
+                "tanh": tanh,
+                "relu": relu}
+    for af, fct in act_fct.items(): 
+        random_size = randrange(2, 20)
+        acf = ac_f.activation_function(af, use_torch=False)
+        tns = rand(random_size)
+        tns_acf = acf(tns)
+        tns_check = tns
+        for i in range(random_size):
+            tns_check[i] = fct(tns[i])
+        assert array_equal(tns_acf, tns_check).all(), "Activation function of the package not working correctly"
